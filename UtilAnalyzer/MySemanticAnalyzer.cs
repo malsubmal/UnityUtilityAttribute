e586e204b9eda1eebf968f083f3034f025803dd9
malsubmal <63635575+malsubmal@users.noreply.github.com>
2025-08-16 12:22:45 +0700

    TODO: test with partial method

--- UtilAnalyzer/MySemanticAnalyzer.cs
+++ UtilAnalyzer/MySemanticAnalyzer.cs
@@ -8,10 +8,7 @@ using Microsoft.CodeAnalysis.Operations;
 
 namespace SampleAnalyzer
 {
-    /// <summary>
-    /// A sample analyzer that reports invalid values being used for the 'speed' parameter of the 'SetSpeed' function.
-    /// To make sure that we analyze the method of the specific class, we use semantic analysis instead of the syntax tree, so this analyzer will not work if the project is not compilable.
-    /// </summary>
+
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class MySemanticAnalyzer : DiagnosticAnalyzer
     {
@@ -25,51 +22,31 @@ namespace SampleAnalyzer
             isEnabledByDefault: true);
 
 
-        // Keep in mind: you have to list your rules here.
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
             ImmutableArray.Create(Rule);
 
         public override void Initialize(AnalysisContext context)
         {
-            // You must call this method to avoid analyzing generated code.
+            //avoid generated code
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
 
-            // You must call this method to enable the Concurrent Execution.
+            //enable concurrent
             context.EnableConcurrentExecution();
 
             context.RegisterOperationBlockStartAction(AnalyzeOperation);
-
-
-            // Check other 'context.Register...' methods that might be helpful for your purposes.
         }
 
-        /// <summary>
-        /// Executed on the completion of the semantic analysis associated with the Invocation operation.
-        /// </summary>
-        /// <param name="context">Operation context.</param>
         private void AnalyzeOperation(OperationBlockStartAnalysisContext context)
         {
-            var methodSymbol = context.OwningSymbol as IMethodSymbol;
-
-            if (methodSymbol is null)
+            if (context.OwningSymbol is not IMethodSymbol methodSymbol)
             {
                 return;
             }
+            
             //check whether the method is an override method
             //check if the base method has the attribute
             //if yes, check if this method call the base method
             //if no, report error
-            
-            bool hasAttributes(ImmutableArray<AttributeData> attributeData, string name)
-            {
-                foreach (var attr in attributeData)
-                {
-                    if (attr.AttributeClass is null) continue;
-                    if (attr.AttributeClass.Name == name) return true;
-                }
-
-                return false;
-            }
 
             bool baseMethodHasAttribute(IMethodSymbol methodSymbol, string name)
             {
@@ -83,10 +60,9 @@ namespace SampleAnalyzer
                 return hasAttributes(methodRef.GetAttributes(), name);
             }
             
-            if (methodSymbol.MethodKind != MethodKind.Ordinary ||
-                !methodSymbol.IsOverride
+            if (methodSymbol.MethodKind != MethodKind.Ordinary 
+                || !methodSymbol.IsOverride
                 || methodSymbol.OverriddenMethod is null
-                //|| //get the base method of the method
                 || !baseMethodHasAttribute(methodSymbol, "MustCallBaseAttribute")
                )
                 return;
@@ -106,13 +82,23 @@ namespace SampleAnalyzer
             {
                 if (!calledBase)
                 {
-                    var decl = methodSymbol.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax() as MethodDeclarationSyntax;
-                    if (decl != null)
+                    if (methodSymbol.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax() is MethodDeclarationSyntax decl)
                     {
                         endContext.ReportDiagnostic(Diagnostic.Create(Rule, decl.Identifier.GetLocation(), methodSymbol.Name));
                     }
                 }
             });
+
+            bool hasAttributes(ImmutableArray<AttributeData> attributeData, string name)
+            {
+                foreach (var attr in attributeData)
+                {
+                    if (attr.AttributeClass is null) continue;
+                    if (attr.AttributeClass.Name == name) return true;
+                }
+
+                return false;
+            }
         }
         
 
